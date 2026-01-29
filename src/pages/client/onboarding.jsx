1890e5c6c2eef75c3e78f2a357257752fc5a3ec2
Rishab Jain <rjain22061998@gmail.com>
2026-01-29 18:52:43 +0530

    link is not defined

--- src/pages/client/onboarding.jsx
+++ src/pages/client/onboarding.jsx
@@ -1,5 +1,5 @@
 import { useAuth, useUser } from "@clerk/clerk-react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
